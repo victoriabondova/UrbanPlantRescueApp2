50b1f9ec17a1c392bb2ff0a73987438b4d04b57c
Victoria Bondova <victoriabondova9@gmail.com>
2026-04-14 21:36:07 +0300

    Add UniqueCategoryAttribute to Data Common folder

---
+++ UrbanPlantRescueApp.Data/Common/UniqueCategoryAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace UrbanPlantRescueApp.Data.Common
+{
+    internal class UniqueCategoryAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            return ValidationResult.Success;
+        }
+    }
+}
