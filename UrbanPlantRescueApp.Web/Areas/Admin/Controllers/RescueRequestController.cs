bf914d437144a077950eeceea61685d43fa069f2
Victoria Bondova <victoriabondova9@gmail.com>
2026-04-16 11:15:36 +0300

    Add Admin Area with RescueRequest management and admin navigation link

---
+++ UrbanPlantRescueApp.Web/Areas/Admin/Controllers/RescueRequestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UrbanPlantRescueApp.Services.Interfaces;
+
+namespace UrbanPlantRescueApp.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class RescueRequestController : Controller
+    {
+        private readonly IRescueRequestService rescueRequestService;
+        public RescueRequestController(IRescueRequestService rescueRequestService)
+        {
+            this.rescueRequestService = rescueRequestService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var requests = await rescueRequestService.GetAllRequestsAsync();
+            return View(requests);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Approve(int requestId)
+        {
+            await rescueRequestService.ApproveRequestAsync(requestId);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
